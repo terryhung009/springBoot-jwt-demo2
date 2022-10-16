1817d43d416623089a41137c3062bdd1a10366aa
Denny <khc741213@gmail.com>
2022-10-16 21:14:18 +0800

    1

--- src/main/java/danny/jwtdemo2/config/SecurityConfig.java
+++ src/main/java/danny/jwtdemo2/config/SecurityConfig.java
@@ -2,20 +2,37 @@ package danny.jwtdemo2.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.*;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    public InMemoryUserDetailsManager user(){
+        return new InMemoryUserDetailsManager(
+                User.withUsername("dvega")
+                        .password("{noop}password")
+                        .authorities("read")
+                        .build()
+        );
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
+        return http
                 .csrf(csrf -> csrf.disable())
                 .authorizeRequests( auth -> auth
                         .anyRequest().authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .httpBasic(withDefaults())
                 .build();
     }
 }
--- src/main/java/danny/jwtdemo2/controller/HomeController.java
+++ src/main/java/danny/jwtdemo2/controller/HomeController.java
@@ -3,11 +3,15 @@ package danny.jwtdemo2.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 @RestController
 public class HomeController {
 
     @GetMapping
-    public String Home(){
-        return "Hello, JWT!";
+    public String Home(Principal principal){
+//        return "Hello, JWT!";
+        return "Hello, " + principal.getName();
+
     }
 }
