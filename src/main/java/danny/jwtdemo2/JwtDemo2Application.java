de4cce8c0ddf73e475fc7684c8c993b03131f674
terryhung009 <76043300+terryhung009@users.noreply.github.com>
2022-10-17 18:42:50 +0800

    1

--- src/main/java/danny/jwtdemo2/JwtDemo2Application.java
+++ src/main/java/danny/jwtdemo2/JwtDemo2Application.java
@@ -1,8 +1,11 @@
 package danny.jwtdemo2;
 
+import danny.jwtdemo2.config.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties(RsaKeyProperties.class)
 @SpringBootApplication
 public class JwtDemo2Application {
 
---
+++ src/main/java/danny/jwtdemo2/config/RsaKeyProperties.java
@@ -0,0 +1,11 @@
+package danny.jwtdemo2.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+@ConfigurationProperties(prefix = "rsa")
+public record RsaKeyProperties(RSAPublicKey publicKey, RSAPrivateKey privateKey) {
+
+}
